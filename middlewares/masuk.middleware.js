2e34fd229988877bbeaebf180eb3b615d5cc77d8
AmriTSalahudin <119931190+amritsalahudin69@users.noreply.github.com>
2023-02-13 15:16:10 +0700

    jwt2
    jwt masih mentok di middleware

---
+++ middlewares/masuk.middleware.js
@@ -0,0 +1,28 @@
+const ErrorResponse = require("../helpers/error.helper")
+const Response = require("../helpers/response.helper")
+const jwt = require('jsonwebtoken')
+
+require('dotenv').config();
+const JWT_Key = process.env.JWT_Key;
+
+function auth (req, res, next){
+    const authHeader = req.headers.authorization;
+    console.log(authHeader);
+    // const token = authHeader(' ')[1];
+    // if(authHeader){
+    //     jwt.verify(token, JWT_Key,(error, User)=>{
+    //         if(error){
+    //             throw new ErrorResponse(403, 'Token Invalid!')
+    //         }
+    //         req.user = User;
+    //         next();
+    //     })
+    // }else{
+    //     throw new ErrorResponse(401, 'Token Invalid!')
+    // }
+    return new Response(res, 200, authHeader);
+}
+
+module.exports= {
+    auth
+}
--- routers/produk.route.js
+++ routers/produk.route.js
@@ -2,8 +2,9 @@ const router = require('express').Router()
 const {produkController} = require('../controllers/produk.controller')
 
 const produkCon = new produkController();
+const authMiddleware = require('../middlewares/masuk.middleware')
 
-router.get('/produk', produkCon.getProduk);
+router.get('/produk', authMiddleware.auth, produkCon.getProduk);
 router.get('/produk/:id', produkCon.getProdukbyId);
 router.post('/produk', produkCon.insertProduk );
 router.put('/produk/:id', produkCon.updateProduk);
